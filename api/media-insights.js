c4a17a6790f92fed5edee3da7323f1f9f19f5e8c
Fer Alvarado <149547904+Fersitrev@users.noreply.github.com>
2026-09-07 10:57:08 -0600

    Update media-insights.js

--- api/media-insights.js
+++ api/media-insights.js
@@ -84,6 +84,8 @@ export default async function handler(req, res) {
     return res.status(200).json({
       data: postsWithInsights
     });
+    
+// endpoint de insights por publicación
 
   } catch (error) {
 
